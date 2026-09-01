b2e4122a2bfd3d154e5645dd05c6ab55a1e44581
juanccvv91 <155189836+juanccvv91@users.noreply.github.com>
2026-09-01 18:15:40 -0500

    Actualizar __init__.py

--- hotmail-checker/checkers/__init__.py
+++ hotmail-checker/checkers/__init__.py
@@ -1,1 +1,6 @@
-# checkers package
+# checkers/__init__.py
+from .crunchyroll import CrunchyrollChecker
+from .paramount import ParamountChecker
+from .hotmail import HotmailChecker
+
+__all__ = ['CrunchyrollChecker', 'ParamountChecker', 'HotmailChecker']
