594c89a4dac6d20831d0f77874af8fa52abd91d7
juanccvv91 <155189836+juanccvv91@users.noreply.github.com>
2026-09-03 00:37:55 -0500

    Eliminar __init__.py

--- hotmail-checker/checkers/__init__.py
+++
@@ -1,6 +0,0 @@
-# checkers/__init__.py
-from .crunchyroll import CrunchyrollChecker
-from .paramount import ParamountChecker
-from .hotmail import HotmailChecker
-
-__all__ = ['CrunchyrollChecker', 'ParamountChecker', 'HotmailChecker']
